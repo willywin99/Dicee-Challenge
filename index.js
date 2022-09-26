1e8275e5ba02ff2eae450f4dffd291b134214352
willywin99 <willywinsianturi@gmail.com>
2022-09-26 18:25:03 +0700

    feat: index.js Add Dice Images

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+// document.querySelectorAll('img')[0].setAttribute('src', 'images/dice6.png');
+// document.querySelectorAll('img')[1].setAttribute('src', 'images/dice6.png');
+// let images = document.querySelectorAll('img');
+// console.log(images);
+
+// document.getElementsByTagName('img')[0].setAttribute('src', 'images/dice6.png');
+// document.getElementsByTagName('img')[1].setAttribute('src', 'images/dice6.png');
+
+document.getElementsByClassName('img1')[0].setAttribute('src', 'images/dice6.png');
+document.getElementsByClassName('img2')[0].setAttribute('src', 'images/dice6.png');
+
+// let images = document.getElementsByTagName('img');
+// console.log(images);
+// images[0].src = 'images/dice6.png';
+// images[1].src = 'images/dice6.png';
