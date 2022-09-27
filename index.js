fd36abc4d1802240813ad87e553601922e6a9fe4
willywin99 <willywinsianturi@gmail.com>
2022-09-27 16:33:55 +0700

    feat: index.js Change the Title to Display a Winner

--- index.js
+++ index.js
@@ -16,9 +16,22 @@ document.getElementsByClassName('img2')[0].setAttribute('src', 'images/dice6.png
 // images[1].src = 'images/dice6.png';
 
 let randomNumber1 = Math.floor(Math.random() * 6 + 1);
-console.log(randomNumber1);
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+// console.log(randomNumber1);
 
 // img1.setAttribute('src', `images/dice${randomNumber1}.png`);
 // img1.src = `images/dice${randomNumber1}.png`;
 
 document.getElementsByClassName('img1')[0].setAttribute('src', `images/dice${randomNumber1}.png`);
+document.getElementsByClassName('img2')[0].setAttribute('src', `images/dice${randomNumber2}.png`);
+
+let heading = document.getElementsByTagName('h1');
+// console.log(heading[0].textContent);
+
+if ( randomNumber1 > randomNumber2) {
+  heading[0].textContent = `Player 1 Wins!`;
+} else if (randomNumber1 < randomNumber2) {
+  heading[0].textContent = `Player 2 Wins!`;
+} else {
+  heading[0].textContent = `Draw!`;
+}
